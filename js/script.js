a0dc7f6c070fc441fd3a329c49aa7f44a7159bb0
Javier Arellano <jomaevan13@gmail.com>
2019-11-17 18:08:01 -0600

    agregando nodos al DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-script.js
+var elementosP = document.getElementById("p");
+var segundoParrafo = document.getElementById("segundo");
+//1.-Crear el elemento
+var elemento = document.createElement("h2");
+//2.-Crear un nodo de texto
+var contenido = document.createTextNode("Este es el titular");
+//3.-Añadir el nodo de texto al elemento
+elemento.appendChild(contenido);
+//4.- Agregar atributos
+elemento.setAttribute("aling", "center");
+//5.- Agregar el elemento al documento
+document.getElementById("subtitulo").appendChild(elemento);
